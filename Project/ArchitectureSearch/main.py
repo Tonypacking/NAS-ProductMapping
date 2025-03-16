d972319e0114797bf7ac352e0d83ddc3398760a5
Tony <vrbaada@gmail.com>
2025-03-16 20:38:19 +0100

    Feat: added parallel and iterations arguments

--- Project/ArchitectureSearch/main.py
+++ Project/ArchitectureSearch/main.py
@@ -2,19 +2,20 @@ import pandas as pd
 from ProMap import ProductsDatasets
 import argparse
 import NeuroEvolution
-parser = argparse.ArgumentParser()
+import NeatConfigParser
 
 def main(args: argparse.Namespace):
 
-    
-
     a = ProductsDatasets.Load_extended_promap_cz()
-
     evolution = NeuroEvolution.Evolution("Config/ProMapCz", a)
-    evolution.run(parralel=True)
+    
+    evolution.run(args.iterations, args.parallel)
 
     print(f"validation of: {evolution.validation()}")
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--parallel', '--par',  action='store_true', default=False, help="Runs Neat in parallel.",)
+    parser.add_argument('--iterations', '--iter', default=50, type=int, help='Number of generations in neat')
     main(parser.parse_args())
 
