b57a75695b2805c4c8fc877980ae96b91029fe28
Tony <vrbaada@gmail.com>
2025-03-17 16:11:36 +0100

    Feat added scaling and dim reduction of dataset

--- Project/ArchitectureSearch/Dataset.py
+++ Project/ArchitectureSearch/Dataset.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import numpy as np
-
+import sklearn.decomposition
+import sklearn.discriminant_analysis
+from sklearn.preprocessing import StandardScaler
+import sklearn
 class Dataset:
     """_summary_
     Dataset wrapper. 
     Splits training and testing data to features and targets
     """
     def __init__(self, training_data: pd.DataFrame, testing_data: pd.DataFrame, name: None| str  = None):
+
+        for col in training_data.columns:
+            print(f"Unique {col} {training_data[col].unique()}")
         self.ids = ['id1', 'id2']
         self.dataset_name = name
         self.feature_labels = training_data.drop(self.ids, axis=1).iloc[:, :-1].columns
@@ -22,4 +28,33 @@ class Dataset:
         self.test_targets = testing_data.iloc[:, -1].values
         self.testing_product_ids = testing_data[self.ids].values
         
-       
+    def scale_features(self):
+        """
+        Standardize features via sklearn.preprocessing.StandardScaler
+        """
+        scaler = StandardScaler()
+        self.train_set = scaler.fit_transform(self.train_set)
+        self.test_set = scaler.transform(self.test_set)
+    
+    def reduce_dimensions(self, method: str = 'lda'):
+        """
+        Reduce dimensions of test and train data
+        LDA will compute maximum number of components based on the number of classes in the dataset.
+        PCA will select the number of components such that the amount of variance that needs to be explained is greater than 95%
+
+        Args:
+            method (str, optional): Reduction method selection. Available dim. reduction is via LDA or PCA. Defaults to 'lda'.
+
+        Raises:
+            ValueError: If method is unknown dimension reduction methon
+        """
+        if method.lower() == 'lda':
+            lda = sklearn.discriminant_analysis.LinearDiscriminantAnalysis()
+            self.train_set = lda.fit_transform(self.train_set)
+            self.test_set = lda.transform(self.test_set)
+        elif method.lower() == 'pca':
+            pca = sklearn.decomposition.PCA(n_components=0.95)
+            self.train_set = pca.fit_transform(self.train_set)
+            self.test_set = pca.transform(self.test_set)
+        else:
+            raise ValueError('Unknows reduction method. Valid reduction method is: lda or pca')
