762aa0fe924d86e6d6f7252af5324f0eeb4536e9
Tony <vrbaada@gmail.com>
2025-03-21 15:31:06 +0100

    Added comments to functions.

--- Project/ArchitectureSearch/NeuroEvolution.py
+++ Project/ArchitectureSearch/NeuroEvolution.py
@@ -34,6 +34,12 @@ class Evolution:
         return 1 if x >= 0.5 else 0
     
     def _eval_genomes(self, genomes, config):
+        """Evaluates genomes
+
+        Args:
+            genomes (_type_): genomes
+            config (_type_): path to config file.
+        """
         for id, genome in genomes:
             genome.fitness = self._dataset.train_set.shape[0]
 
@@ -43,6 +49,16 @@ class Evolution:
             genome.fitness = sklearn.metrics.f1_score(y_pred=predicted, y_true=self._dataset.train_targets) * self._fitness_scaling
 
     def _create_config(self, config_path, scaling : bool = False, dimension_reduction: str = 'raw') -> neat.Config:
+        """Creates config for genomes. If dataset's dimensions is reduced, it updates neat's input nodes.
+
+        Args:
+            config_path (_type_): Path to config file
+            scaling (bool, optional): Scales features through StandardScaler before running neat. Defaults to False.
+            dimension_reduction (str, optional): Reduces dimensions before running neat. Defaults to 'raw'.
+
+        Returns:
+            neat.Config: Neat configuration.
+        """
         if scaling:
             self._scaler = self._dataset.scale_features()
 
@@ -61,7 +77,15 @@ class Evolution:
         return neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
 
     def run(self, iterations: int = 50, parralel: bool = False) -> neat.nn.FeedForwardNetwork:
+        """Runs the fining algorithm using neat.
 
+        Args:
+            iterations (int, optional): Number of generations in neat. Defaults to 50.
+            parralel (bool, optional): Parralel evaluation of genomes. Defaults to False.
+
+        Returns:
+            neat.nn.FeedForwardNetwork: _description_
+        """
         population = neat.Population(self._neat_config)
 
         population.add_reporter(neat.StdOutReporter(show_species_detail=True))
@@ -79,6 +103,15 @@ class Evolution:
         return self.Best_network
     
     def validate(self, test_set: Optional[Sequence] = None , target_set: Optional[Sequence] = None) -> dict[str, float]:
+        """Va;odates best network against unseen data.
+
+        Args:
+            test_set (Optional[Sequence], optional): testing set. Defaults to None.
+            target_set (Optional[Sequence], optional): testing true outpiut. Defaults to None.
+
+        Returns:
+            dict[str, float]: dictionary of name of a metric and metric's value
+        """
         if test_set is None and target_set is None:
             predicted = np.array([Evolution._binarize_prediction(self.Best_network.activate(x)[0]) for x in self._dataset.test_set])
             target_set = self._dataset.test_targets
@@ -98,6 +131,11 @@ class Evolution:
         }
 
     def validate_all(self) -> list[tuple[str, dict[str, float]]]:
+        """Validates against all promap datasets if feature count is the same.
+
+        Returns:
+            list[tuple[str, dict[str, float]]]: List of tuples with name of tested dataset and dictionary of metric and metrics value.
+        """
         outputs = []
         for name in ProMap.ProductsDatasets.NAME_MAP:
             dataset= ProMap.ProductsDatasets.Load_by_name(name)
@@ -118,11 +156,29 @@ class Evolution:
         return outputs
     
     def plot_network(self, save_path :str, view = False ):
+        """Plots the best genome's network
+
+        Args:
+            save_path (str): save path
+            view (bool, optional): View of a plot durring runtime. Defaults to False.
+
+        Returns:
+            None: None
+        """
         if self.Best_network is None:
             return # nothing to vizualize
         visualize.draw_net(config=self._neat_config,genome=self._winner, view=view, filename=save_path)
         
     def plot_statistics(self, save_path :str, view = False ):
+        """Plots neat staticstics
+
+        Args:
+            save_path (str): save path
+            view (bool, optional): View of a plot durring runtime. Defaults to False.
+
+        Returns:
+            None: None
+        """
         if self._statistics is None:
             return # nothing to vizualize
         visualize.plot_stats(self._statistics, filename=save_path, view=view)
