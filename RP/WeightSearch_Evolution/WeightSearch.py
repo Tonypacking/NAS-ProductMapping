513025bdcea8abf8d629dec6344dc66810a5b6f1
Tony <vrbaada@gmail.com>
2025-03-28 17:07:16 +0100

    Updated Validation functions.

--- RP/WeightSearch_Evolution/WeightSearch.py
+++ RP/WeightSearch_Evolution/WeightSearch.py
@@ -6,10 +6,10 @@ import pickle
 import os
 import sys
 from functools import partial
-import sklearn.metrics
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..'))) # To load Utils module
-
 from deap import creator, algorithms, base, tools, cma
+import sklearn.metrics
+import sklearn.neural_network
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..'))) # To load Utils module
 from Utils.ProMap import ProductsDatasets
 
 class EvolutionaryNeuronNetwork:
@@ -17,8 +17,17 @@ class EvolutionaryNeuronNetwork:
     Wrapper of neural network. Provides simple api changing weight of NN. 
     """
     def __init__(self, args: argparse.Namespace, hidden_layer_size: tuple[int]):
-        self._nn = sklearn.neural_network.MLPClassifier(hidden_layer_sizes=hidden_layer_size, max_iter=1) # set max iter to 1, so biases and weight shapes are defined
+        self._nn = sklearn.neural_network.MLPClassifier(hidden_layer_sizes=hidden_layer_size, max_iter=1, verbose=False) # set max iter to 1, so biases and weight shapes are defined
         self._dataset = ProductsDatasets.Load_by_name(args.dataset)
+        self._transformer = None
+        self._scaler = None
+
+        if args.scale:
+            self._scaler = self._dataset.scale_features()
+
+        if args.dimension_reduction:
+            self._transformer = self._dataset.reduce_dimensions(method=args.dimension_reduction)
+
         self._nn.fit(self._dataset.train_set, self._dataset.train_targets)
         self.n_parameters = self._parameter_count()
         self._metrics = self._choose_metric(args.metrics)
@@ -70,7 +79,7 @@ class EvolutionaryNeuronNetwork:
         params = 0
         for w, b in zip(self._nn.coefs_, self._nn.intercepts_):
             params += w.size + b.size  
-
+        
         return params
 
     def change_weights(self, weights: list):
@@ -112,32 +121,52 @@ class EvolutionaryNeuronNetwork:
         # f1 = sklearn.metrics.f1_score(y_true= self._dataset.train_targets, y_pred=pred, average='weighted')
         return self._metrics(y_true= self._dataset.train_targets, y_pred=pred)
     
-    def validate(self) -> float:
-        """Validates neuron network.
-
-        Returns:
-            float: f1_score of a NN.
-        """
-        pred = self._nn.predict(self._dataset.test_set)
-        return sklearn.metrics.f1_score(y_true=self._dataset.test_targets, y_pred=pred) 
-
-    def validate_all(self) -> dict[str, float]:
+    def validate(self, test_set = None, test_targets = None) -> dict[str, float]:
         """Validates NN againsts f1 score (binary, macro, micro and weighted average), precision, recall, accuracy and confusion matrix
 
         Returns:
             dict[str, float]: Dictionary of metric's name and metric's validation.
         """
-        pred = self._nn.predict(self._dataset.test_set)
+        if test_set is None or test_targets is None:   
+            pred = self._nn.predict(self._dataset.test_set)
+            test_targets = self._dataset.test_targets
+        else:
+            pred = self._nn.predict(test_set)
+
         return {
-            'f1_score_binary' : sklearn.metrics.f1_score(y_true=self._dataset.test_targets, y_pred=pred, average="binary"),
-            'f1_score_macro' : sklearn.metrics.f1_score(y_true=self._dataset.test_targets, y_pred=pred, average="macro"),
-            'f1_score_micro' : sklearn.metrics.f1_score(y_true=self._dataset.test_targets, y_pred=pred, average="micro"),
-            'f1_score_weighted' : sklearn.metrics.f1_score(y_true=self._dataset.test_targets, y_pred=pred, average="weighted"),
-            "precision" : sklearn.metrics.precision_score(y_true=self._dataset.test_targets, y_pred=pred),
-            "recall" : sklearn.metrics.recall_score(y_true=self._dataset.test_targets, y_pred=pred),
-            'accuracy' : sklearn.metrics.accuracy_score(y_true=self._dataset.test_targets, y_pred=pred),
-            'confusion_matrix' : sklearn.metrics.confusion_matrix(y_true=self._dataset.test_targets, y_pred=pred),
+            'f1_score_binary' : sklearn.metrics.f1_score(y_true=test_targets, y_pred=pred, average="binary"),
+            'f1_score_macro' : sklearn.metrics.f1_score(y_true=test_targets, y_pred=pred, average="macro"),
+            'f1_score_micro' : sklearn.metrics.f1_score(y_true=test_targets, y_pred=pred, average="micro"),
+            'f1_score_weighted' : sklearn.metrics.f1_score(y_true=test_targets, y_pred=pred, average="weighted"),
+            "precision" : sklearn.metrics.precision_score(y_true=test_targets, y_pred=pred),
+            "recall" : sklearn.metrics.recall_score(y_true=test_targets, y_pred=pred),
+            'accuracy' : sklearn.metrics.accuracy_score(y_true=test_targets, y_pred=pred),
+            'confusion_matrix' : sklearn.metrics.confusion_matrix(y_true=test_targets, y_pred=pred),
         }
+
+    def validate_all(self) -> dict[str, float]:
+        """Validates against all promap datasets if feature count is the same.
+
+        Returns:
+            list[tuple[str, dict[str, float]]]: List of tuples with name of tested dataset and dictionary of metric and metrics value.
+        """
+
+        outputs = []
+        for name in ProductsDatasets.NAME_MAP:
+            dataset= ProductsDatasets.Load_by_name(name)
+
+            if dataset.feature_labels.shape != self._dataset.feature_labels.shape:   
+                print(f'Datasets features are different, cannot transform them\nTested dataset name: {dataset.dataset_name} of shape {dataset.feature_labels.shape}\nTrained on {self._dataset.dataset_name} of shape {self._dataset.feature_labels.shape}')
+                continue
+
+            if self._scaler:
+                dataset.test_set = self._scaler.transform(dataset.test_set)
+                
+            if self._transformer:
+                dataset.test_set = self._transformer.transform(dataset.test_set)
+
+            outputs.append((dataset.dataset_name, self.validate(dataset.test_set, dataset.test_targets)))
+        return outputs
 
     def save_network(self, save_path: str):
         """Saves a NN.
@@ -161,7 +190,7 @@ class EvolutionaryNeuronNetwork:
         with open(model_path, 'rb') as model:
              self._nn = pickle.load(model)
 
-class WeightSearch:
+class Evo_WeightSearch:
     """Main algorithm for searching weights in NN via evolution algorithms.
     """
     def __init__(self, args: argparse.Namespace):
@@ -172,11 +201,11 @@ class WeightSearch:
         creator.create("Individual", list, fitness=creator.FitnessMax)
 
         self._toolbox = base.Toolbox()
-        self._toolbox.register("evaluate", lambda ind: WeightSearch._fitness(self._neuron_network, ind))
-        self._toolbox.register("mutate", tools.mutGaussian, mu=0.0, sigma=0.1, indpb=0.2)
-        self._toolbox.register("mutate_polynomial", tools.mutPolynomialBounded, eta=20.0, indpb=0.2)
-        self._toolbox.register("mutate_lognormal", tools.mutESLogNormal, mu=0.0, sigma=0.1, indpb=0.2)
+        self._toolbox.register("evaluate", lambda ind: Evo_WeightSearch._fitness(self._neuron_network, ind))
+        self._toolbox.register("select", tools.selTournament, tournsize=3)
         
+        self._toolbox.register("mutate", tools.mutGaussian, mu=0.0, sigma=0.1, indpb=0.2)
+
         strategy = cma.Strategy(centroid=[0.1] * self._neuron_network.n_parameters, sigma=0.5, lambda_= 8 * self._neuron_network.n_parameters)
         self._toolbox.register("generate", strategy.generate, creator.Individual)
         self._toolbox.register("update", strategy.update)
@@ -213,5 +242,5 @@ class WeightSearch:
         """
         (pop, stats) = algorithms.eaGenerateUpdate(self._toolbox, ngen=generations, stats=self._stats, halloffame=self._hall_of_fame)
         self._neuron_network.change_weights(weights=self._hall_of_fame[0])
-        pprint.pprint(self._neuron_network.validate_all())
+        # pprint.pprint(self._neuron_network.validate_all())
         self._neuron_network.save_network(save_path=self._save_path) 
