5067608f006656cfd4300cfe195ce7df7659ee6c
Tony <vrbaada@gmail.com>
2025-04-04 11:58:34 +0200

    Feat: Datasets now enables adding missing freatures from other datasets.

--- Utils/ProMap.py
+++ Utils/ProMap.py
@@ -52,33 +52,49 @@ class ProductsDatasets:
                 raise ValueError(f'Unknown dataset: {name}')
     
     @staticmethod
-    def __Split_data(path:str, dataset_name: str) -> Dataset:
-        """_summary_
-        Helper function to load and split the data into train and test data.
-        Args:
-            path (str): prefix path to the dataset
-            dataset_name (str): dataset name
-
-        Returns:
-            Dataset: train and test set
+    def __Split_data(path:str, dataset_name: str, extend_to : str|None) -> Dataset:
         """
+        Helper function to load a dataset and split it into training and testing sets.
+
+        Args:
+            path (str): The base directory path where the dataset is stored.
+            dataset_name (str): The name of the dataset to be loaded.
+            extend_to (str | None): Optional. Path to an additional dataset (e.g., 'promap') used to fill missing columns in the main dataset (`dataset_name`). Missing columns will be filled with zeros based on the `extend_to` dataset. If not provided, no columns will be added.
+
+        Returns: Dataset wrapper with proper train and test sets 
+        """
+
+
         train_suffix = f"{dataset_name}-train_data_similarities.csv"
         test_suffix = f"{dataset_name}-test_data_similarities.csv"
         test_data = pd.read_csv(os.path.join(path, test_suffix))
         train_data = pd.read_csv(os.path.join(path, train_suffix))
+
+        if extend_to:
+            extended_train_data = pd.read_csv(f"{extend_to}-train_data_similarities.csv")
+
+            missing_columns = set(extended_train_data.columns) - set(train_data.columns) # set label intersection. Find missins labels.
+            # Extend missing features. Fill them by zero
+            for col in missing_columns:
+                test_data[col] = 0
+                train_data[col] = 0
+
+            # Reorder the features so the features for testing datasets align.
+            train_data = train_data[extended_train_data.columns]
+            test_data = test_data[extended_train_data.columns]
         return Dataset(train_data, test_data, dataset_name)
     
-    @staticmethod
-    def Load_basic_amazon_google(path: str = os.path.join(_default_basic_path,"amazon-google")) -> Dataset:
-        """Loads amazon google dataset
+    # @staticmethod
+    # def Load_basic_amazon_google(path: str = os.path.join(_default_basic_path,"amazon-google")) -> Dataset:
+    #     """Loads amazon google dataset
 
-        Args:
-            path (str, optional): _description_. Path to the promap dataset
+    #     Args:
+    #         path (str, optional): _description_. Path to the promap dataset
 
-        Returns:
-            Dataset: Dataset encapsulation.
-        """
-        return ProductsDatasets.__Split_data(path, "amazon_google")
+    #     Returns:
+    #         Dataset: Dataset encapsulation.
+    #     """
+    #     return ProductsDatasets.__Split_data(path, "amazon_google", extend_to=)
     # @staticmethod
     # def __Split_data(path:str, dataset_name: str) -> Dataset:
     #     """_summary_
@@ -97,7 +113,7 @@ class ProductsDatasets:
     #     return Dataset(train_data, test_data, dataset_name)
     
     @staticmethod
-    def Load_basic_amazon_google(path: str = os.path.join(_default_basic_path,"amazon-google")) -> Dataset:
+    def Load_basic_amazon_google(extend_to: str|None = os.path.join(_default_basic_path,"ProMapCz", 'promapcz'), path: str = os.path.join(_default_basic_path,"amazon-google")) -> Dataset:
         """Loads amazon google dataset
 
         Args:
@@ -106,10 +122,11 @@ class ProductsDatasets:
         Returns:
             Dataset: Dataset encapsulation.
         """
-        return ProductsDatasets.__Split_data(path, "amazon_google")
+
+        return ProductsDatasets.__Split_data(path, "amazon_google", extend_to=extend_to)
     
     @staticmethod
-    def Load_basic_amazon_walmart(path:str = os.path.join(_default_basic_path, "amazon-walmart") ) -> Dataset:
+    def Load_basic_amazon_walmart(extend_to: str|None = os.path.join(_default_basic_path,"ProMapCz", 'promapcz'), path:str = os.path.join(_default_basic_path, "amazon-walmart") ) -> Dataset:
         """Loads amazon walmart dataset
 
         Args:
@@ -118,10 +135,10 @@ class ProductsDatasets:
         Returns:
             Dataset: Dataset encapsulation.
         """
-        return ProductsDatasets.__Split_data(path, "amazon_walmart")
+        return ProductsDatasets.__Split_data(path, "amazon_walmart", extend_to=extend_to)
 
     @staticmethod
-    def Load_basic_promap_cz(path :str =  os.path.join(_default_basic_path,"ProMapCz")  ) -> Dataset:
+    def Load_basic_promap_cz(extend_to: str|None = None, path :str =  os.path.join(_default_basic_path,"ProMapCz")  ) -> Dataset:
         """Loads promap cz dataset
 
         Args:
@@ -130,10 +147,10 @@ class ProductsDatasets:
         Returns:
             Dataset: Dataset encapsulation.
         """
-        return ProductsDatasets.__Split_data(path, "promapcz")
+        return ProductsDatasets.__Split_data(path, "promapcz", extend_to=extend_to)
 
     @staticmethod
-    def Load_basic_promap_en(path:str = os.path.join(_default_basic_path, "ProMapEn") ) -> Dataset:
+    def Load_basic_promap_en(extend_to: str|None = None, path:str = os.path.join(_default_basic_path, "ProMapEn") ) -> Dataset:
         """Loads promap en dataset
 
         Args:
@@ -142,10 +159,10 @@ class ProductsDatasets:
         Returns:
             Dataset: Dataset encapsulation.
         """
-        return ProductsDatasets.__Split_data(path, "promapen")
+        return ProductsDatasets.__Split_data(path, "promapen", extend_to=extend_to)
 
     @staticmethod
-    def Load_extended_promap_cz(path: str = _default_extended_path) -> Dataset:
+    def Load_extended_promap_cz( extend_to :str | None = None, path: str = _default_extended_path) -> Dataset:
         """Loads extended promap cz  dataset
 
         Args:
@@ -154,10 +171,10 @@ class ProductsDatasets:
         Returns:
             Dataset: Dataset encapsulation.
         """
-        return ProductsDatasets.__Split_data(path, "promapczext")
+        return ProductsDatasets.__Split_data(path, "promapczext", extend_to= extend_to)
 
     @staticmethod
-    def Load_extended_promap_en(path: str = _default_extended_path) -> Dataset:
+    def Load_extended_promap_en(extend_to: str|None = None, path: str = _default_extended_path) -> Dataset:
         """Loads extended promap en  dataset
 
         Args:
@@ -166,10 +183,10 @@ class ProductsDatasets:
         Returns:
             Dataset: Dataset encapsulation.
         """
-        return ProductsDatasets.__Split_data(path, "promapenext")
+        return ProductsDatasets.__Split_data(path, "promapenext", extend_to=extend_to)
     
     @staticmethod
-    def Load_extended_amazon_walmart(path: str = _default_extended_path) -> Dataset:
+    def Load_extended_amazon_walmart(extend_to: str|None = os.path.join(_default_basic_path,"ProMapCz", 'promapcz'), path: str = _default_extended_path) -> Dataset:
         """Loads extended amazon walmart dataset
 
         Args:
@@ -178,6 +195,6 @@ class ProductsDatasets:
         Returns:
             Dataset: Dataset encapsulation.
         """
-        return ProductsDatasets.__Split_data(path, "promapmulti_amazon_ext")
+        return ProductsDatasets.__Split_data(path, "promapmulti_amazon_ext", extend_to=extend_to)
     
     
