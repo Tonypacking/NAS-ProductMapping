7977231986477a5424f871976a5126ea64811f0a
Tony <vrbaada@gmail.com>
2025-06-08 22:29:06 +0200

    Fix: fixed module path

--- BP/ArchitectureSearch/HyperNEAT/__init__.py
+++ BP/ArchitectureSearch/HyperNEAT/__init__.py
@@ -1,7 +1,7 @@
 # import pureples.shared as shared 
 # import pureples.hyperneat as hyperneat
 # import pureples.es_hyperneat as es_hyperneat
-import shared as shared 
-import hyperneat as hyperneat
-import es_hyperneat as es_hyperneat
+import HyperNEAT.shared as shared  
+import HyperNEAT.hyperneat as hyperneat
+import HyperNEAT.es_hyperneat as es_hyperneat
 
--- BP/ArchitectureSearch/HyperNEAT/shared/__init__.py
+++ BP/ArchitectureSearch/HyperNEAT/shared/__init__.py
@@ -2,7 +2,7 @@
 # from pureples.shared.gym_runner import run_es, run_hyper, run_neat 
 # from pureples.shared.visualize import draw_net, draw_pattern, draw_es
 # from pureples.shared.substrate import Substrate
-from create_cppn import create_cppn
-from gym_runner import run_es, run_hyper, run_neat 
-from visualize import draw_net, draw_pattern, draw_es
-from substrate import Substrate
+from HyperNEAT.shared.create_cppn import create_cppn
+from HyperNEAT.shared.gym_runner import run_es, run_hyper, run_neat 
+from HyperNEAT.shared.visualize import draw_net, draw_pattern, draw_es
+from HyperNEAT.shared.substrate import Substrate
